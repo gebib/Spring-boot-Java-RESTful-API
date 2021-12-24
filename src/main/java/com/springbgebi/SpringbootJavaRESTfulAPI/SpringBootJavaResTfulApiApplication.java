9fc342742903b61cb4010d4eacd9c049988470ec
GEbi Beshir <gebi9@hotmail.com>
2021-12-24 01:07:22 +0100

    Inited: folder structure + DB connection ok

---
+++ src/main/java/com/springbgebi/SpringbootJavaRESTfulAPI/SpringBootJavaResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.springbgebi.SpringbootJavaRESTfulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbgebi/SpringbootJavaRESTfulAPI/SpringBootJavaResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbgebi.SpringbootJavaRESTfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
