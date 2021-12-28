35269a3880e6b772c9d152628fb3f3a9f929acbc
gebib <gebi9@hotmail.com>
2021-12-28 11:18:29 +0100

    All working

--- src/main/java/com/springbgebi/SpringbootJavaRESTfulAPI/SpringBootJavaResTfulApiApplication.java
+++ src/main/java/com/springbgebi/SpringbootJavaRESTfulAPI/SpringBootJavaResTfulApiApplication.java
@@ -1,36 +1,11 @@
 package com.springbgebi.SpringbootJavaRESTfulAPI;
-
-import com.springbgebi.SpringbootJavaRESTfulAPI.repository.ListDataRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootJavaResTfulApiApplication implements CommandLineRunner {
 
+@SpringBootApplication
+public class SpringBootJavaResTfulApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJavaResTfulApiApplication.class, args);
-    }
-
-
-    @Autowired
-    private ListDataRepository listDataRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-        /*
-        ListDataModel listDataModel = new ListDataModel();
-        listDataModel.setFirstName("Gebi");
-        listDataModel.setLastName("Beshir");
-        listDataModel.setEmailId("gebi@gmgm.com");
-        listDataRepository.save(listDataModel);
-
-        ListDataModel listDataModel1 = new ListDataModel();
-        listDataModel1.setFirstName("Kalid");
-        listDataModel1.setLastName("Beshir");
-        listDataModel1.setEmailId("kald@gmgm.com");
-        listDataRepository.save(listDataModel1);
-         */
     }
 }
